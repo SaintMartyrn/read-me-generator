be79e07b924c8c5adee58daeb2e9f62e712b9cd5
SaintMartyrn <dartagnanhickey@.com>
2023-01-18 21:43:31 -0800

    added functions to js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,50 @@
+function renderContributingSection(confirmContributers, data) {
+  if (!confirmContributers) {
+    return `
+  Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
+    `;
+  } else {
+    return `
+  ${data}
+    `;
+  }
+}
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if (license !== 'no license') {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+  return `
+  [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseTOC(license) {
+  if (license !== 'no license') {
+  return `
+  * [License](#license)
+    `;
+  } else {
+    return ' ';
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
